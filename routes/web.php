6db838c39c9097964e2d44e4e03bb8500a83e95e
Rinda Faiz Shabira <rindaf1@gmail.com>
2021-11-18 19:10:32 +0700

    Kuliah 18 Nov 21

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,11 @@ Route::get('praktikum2', function () {
 Route::get('ets', 'ViewController@showEts');
 Route::post('convert', 'ViewController@convertfunc');
 
-
+//route CRUD
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
 
